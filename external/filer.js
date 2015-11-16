ae4726d607f5dd696da38b733fed38409e4d113f
alpine9000 <alex.g.brown@gmail.com>
2015-11-16 13:23:55 +1100

    Revert "Debugging code"
    This reverts commit 6682493657447f61f45257344ce78152a3da9aca.

--- external/filer.js
+++ external/filer.js
@@ -466,7 +466,6 @@ var Filer = new function() {
    */
   Filer.prototype.init = function(opt_initObj, opt_successCallback,
                                   opt_errorHandler) {
-    console.log("Filer.prototype.init")
     if (!self.requestFileSystem) {
       throw new MyFileError({
         code: FileError.BROWSER_NOT_SUPPORTED,
@@ -488,7 +487,6 @@ var Filer = new function() {
       cwd_ = fs_.root;
       isOpen_ = true;
 
-      console.log("Filer.prototype.init -> opt_successCallback")
       opt_successCallback && opt_successCallback(fs);
     };
 
