0739b4e31a97a3a7dfba3576f9b19e17270b79e7
alpine9000 <alex.g.brown@gmail.com>
2015-12-05 18:36:44 +1100

    Update io.js

--- io.js
+++ io.js
@@ -181,7 +181,7 @@ var kernel = {
         var data = "", c;
         
         for (; c !== 0 && (address-baseAddress) < 1024; address++) {
-            c = kernel.Read8(address);
+            c = kernel.ReadRam8(address);
             if (c !== 0) {
                 data += String.fromCharCode(c);
             }
